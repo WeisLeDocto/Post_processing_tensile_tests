e6d3059a615f2f43927d3787f254073ba866c4fa
Antoine Weisrock <antoine.weisrock@gmail.com>
2022-02-09 15:07:56 +0100

    [add] Added a __main__ file containing, for now, the ToDo

---
+++ post_process/__main__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+# Todo:
+#   Graphical interface using PyQt5
+#   Select files: either all in one file or separate files
+#   Then select either folders or files
+#   Need to select the channels: position, force, strain x and y: give a name
+#   for each channel
+#   The name for each test is the one of the file or folder according to the
+#   chosen mode
+#   Plot the 2 or 3 curves together, and select the beginning and end with
+#   cursors (manipulate cursors with mouse ?)
+#   If separate files, multiplex them
+#   Select the desired post-processing, with the corresponding options
+#   After processing, select the desired output (excel report, curves,
+#   multiplexed data, etc.)
+#   View result separately or all together
+#   Possibility to modify the beginning and end -> menu with the tests ?
